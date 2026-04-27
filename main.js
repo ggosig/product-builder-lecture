57b8d0b27565acf76fd172aea5808808f715687e
ggosigggong <ggosigggong@gmail.com>
2026-04-27 09:58:28 +0000

    Redesign page to Fullmetal Alchemist Character Bio site

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-const generatorBtn = document.getElementById('generator-btn');
-const numberContainers = document.querySelectorAll('.number');
 const themeBtn = document.getElementById('theme-btn');
 const body = document.body;
 
@@ -22,27 +20,3 @@ themeBtn.addEventListener('click', () => {
         themeBtn.textContent = 'Dark Mode';
     }
 });
-
-generatorBtn.addEventListener('click', () => {
-    const lottoNumbers = [];
-    while (lottoNumbers.length < 6) {
-        const randomNumber = Math.floor(Math.random() * 45) + 1;
-        if (!lottoNumbers.includes(randomNumber)) {
-            lottoNumbers.push(randomNumber);
-        }
-    }
-
-    numberContainers.forEach((container, index) => {
-        container.textContent = lottoNumbers[index];
-        container.style.backgroundColor = getRandomColor();
-    });
-});
-
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
